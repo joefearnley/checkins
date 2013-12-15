9af503c34caefc0be5214485a70f834cf69905c2
Joe Fearnley <joe.fearnley@gmail.com>
2013-12-15 16:26:42 -0500

    Formatting sample config file

--- checkins-config-sample.php
+++ checkins-config-sample.php
@@ -1,7 +1,7 @@
 <?php
 
 $checkins_config = [
-    'client_id'     => 'your_foursquare_client_id',
+    'client_id' => 'your_foursquare_client_id',
     'client_secret' => 'your_foursquare_client_secret',
     'auth_token' => 'your_foursquare_app_auth_token'
 ];
